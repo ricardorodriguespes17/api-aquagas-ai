3f04e43ee0b981a7069dbcb1bd1a718d29c983e3
Ricardo Rodrigues <ricardor662@gmail.com>
2024-08-30 18:38:46 -0300

    configure measure repository

---
+++ src/@types/measureRepositoryTypes.ts
@@ -0,0 +1,19 @@
+export type MeasureCreateType = {
+  customer_code: string
+  datetime: Date
+  image_url: string
+  value: number
+  type: string
+}
+
+export type MeasureUpdateType = {
+  uuid: string
+} & MeasureCreateType
+
+export type MeasureFindByCustomerCode = {
+  customer_code: string
+}
+
+export type MeasureFindByUuid = {
+  uuid: string
+}
---
+++ src/repository/measureRepository.ts
@@ -0,0 +1,31 @@
+import { PrismaClient } from "@prisma/client"
+import { MeasureCreateType, MeasureFindByCustomerCode, MeasureFindByUuid, MeasureUpdateType } from "../@types/measureRepositoryTypes"
+import { prismaClient } from "../services/prismaClient"
+
+const findAll = async () => {
+  return await prismaClient.measure.findMany()
+}
+
+const findByUuid = async (data: MeasureFindByUuid) => {
+  return await prismaClient.measure.findUnique({ where: { uuid: data.uuid } })
+}
+
+const findByCustomerCode = async (data: MeasureFindByCustomerCode) => {
+  return await prismaClient.measure.findFirst({ where: { customer_code: data.customer_code } })
+}
+
+const create = async (data: MeasureCreateType) => {
+  return await prismaClient.measure.create({ data })
+}
+
+const update = async (data: MeasureUpdateType) => {
+  return await prismaClient.measure.update({ data, where: { uuid: data.uuid } })
+}
+
+export {
+  findAll,
+  findByCustomerCode,
+  findByUuid,
+  create,
+  update
+}
