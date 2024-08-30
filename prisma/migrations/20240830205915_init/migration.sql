9e762c4a3f5eb041f34cee7efb12a275e780ca13
Ricardo Rodrigues <ricardor662@gmail.com>
2024-08-30 18:03:22 -0300

    config prisma postgres

---
+++ prisma/migrations/20240830205915_init/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Measure" (
+    "uuid" UUID NOT NULL,
+    "customer_code" TEXT NOT NULL,
+    "datetime" TIMESTAMP(3) NOT NULL,
+    "imageUrl" TEXT NOT NULL,
+    "value" DOUBLE PRECISION NOT NULL,
+    "type" TEXT NOT NULL,
+
+    CONSTRAINT "Measure_pkey" PRIMARY KEY ("uuid")
+);
